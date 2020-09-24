68c4c73c53c6ae53c3de3e7dd168244b95407b36
Christopher Demahy <demahyc@gmail.com>
2020-09-24 18:17:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min = name_hash.first
   name_hash.each do |key, value|
     if value < min[1]
-      min[0] = key 
+      min[0] = key
       min[1] = value
     end
   end
